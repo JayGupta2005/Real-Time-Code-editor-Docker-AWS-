ffa7f4a70373eaefbe2f2cdd3e423d2d76ae16b3
Jay Gupta <jaygupta4520@gmail.com>
2026-09-19 12:03:49 +0530

    backend files add

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import {Editor} from '@monaco-editor/react'
 const App = () => {
   return (
     <main className = "h-screen bg-gray-950 flex gap-3 p-2">
       <aside className = "h-full w-1/4 bg-amber-50 rounded-lg"></aside>
-      <section className = "h-full w-3/4 bg-neutral-800 rounded-lg"></section>
-
+      <section className = "h-full w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+        <Editor height="100%" defaultLanguage="javascript" defaultValue="//Take your first steps toword" theme="vs-dark"/>
+      </section>
     </main>
   )
 }
