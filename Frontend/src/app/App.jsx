c2a90ac81fab7b474eb7bea6395f60a95c8ba8de
Jay Gupta <jaygupta4520@gmail.com>
2026-09-19 11:14:12 +0530

    first commit

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <main className = "h-screen bg-gray-950 flex gap-3 p-2">
+      <aside className = "h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className = "h-full w-3/4 bg-neutral-800 rounded-lg"></section>
+
+    </main>
+  )
+}
+
+export default App
